ba9a0891d44556a17b3926c934fd628dc7375dff
KE-C02RC5YHFVH3 <ke-c02rc5yhfvh3@KE-C02RC5YHFVH3s-MacBook-Pro.local>
2018-11-12 15:24:26 +0300

    [Feature] setup django envrinoment

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ke-c02rc5yhfvh3/Documents/DJango Projects/fisrt_app/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
